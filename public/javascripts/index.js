e1abedda7f5af7b58b3b091ebde5e45605532b1d
Cedric Kisema <cedrickisema@digitalhmrc.gov.uk>
2021-04-20 10:17:44 +0100

    adds live previer

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,15 +1,19 @@
 var iterator = require("markdown-it-for-inline");
 const md = require("markdown-it")({ linkify: true })
- .use(iterator, 'url_new_win', 'link_open', function (tokens, idx) {
-              tokens[idx].attrPush([ 'target', '_blank' ]);
-            });
+  .use(iterator, "url_new_win", "link_open", function (tokens, idx) {
+    tokens[idx].attrPush(["target", "_blank"]);
+  })
+  .use(iterator, "add_class_to_list", "link_open", function (tokens, idx) {
+    tokens[idx].attrPush(["target", "_blank"]);
+  });
 
 document
   .getElementById("writerView")
   .addEventListener("keypress", function (e) {
     let value = document.getElementById("writerView").value;
     if (e.key === "Enter") {
-      document.getElementById("readerView").innerText = md.render(value);
+      document.getElementById("readerHtml").innerText = md.render(value);
+      document.getElementById("readerView").innerHTML = md.render(value);
       console.log("enter was pressed");
     }
   });
